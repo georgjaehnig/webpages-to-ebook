8e98d97adb5942c80e4c3a74d712c99edb5a5f35
Georg Jaehnig <georg@jaehnig.org>
2017-11-16 21:28:57 +0000

    Warn only.

--- index.js
+++ index.js
@@ -70,9 +70,7 @@ function parseFile(url_md5) {
 
     readability(html, function(err, article, meta) {
 			if (err) {
-				console.log(url_md5 + ': Error on parsing. Source URL may be empty.')	
-				console.log('Aborting.')	
-				process.exit();
+				console.log(url_md5 + ': Warning: Error on parsing. Source URL may be empty.');
 			}
       let articleCopy = {};
       articleCopy.title = article.title;
